cceefb3efd2df7bc943a0b2234a6c3f438d7a84f
Aaron Nguyen and Marcel Nogueira <marcel@>
2017-03-22 11:49:17 -0700

    add shout function to form

---
+++ shout/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val().toUpperCase();
+
+    $(".person1").text(person1Input);
+
+
+    event.preventDefault();
+  });
+});
